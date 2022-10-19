63198fc0d0f2b903dadcb363f1c5b93d15664e1b
syo tokeshi <tokeshi@mfgnt.com>
2022-10-19 19:00:21 +0900

    fix

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,31 +1,55 @@
 <template>
-  　<!-- ★ここから追記 -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input v-model="title"
            type="text"
            class="text-input"
            placeholder="Add new list"
+           @focusin="startEditing"
+           @focusout="finishEditing"
     >
-    <button type="submit" class="add-button">
+    <button type="submit"
+            class="add-button"
+            v-if="isEditing || this.classList.titleExists">
       Add
     </button>
   </form>
-  <!-- ★ここまで追記 -->
 </template>
 
 <script>
 export default {
-  
   data: function() {
     return {
       title: '',
+      isEditing: false,
     }
+  },
+  computed: {
+    classList() {
+      const classList = ['addlist']
+
+      if (this.isEditing) {
+        classList.push('active')
+      }
+      if (this.titleExists) {
+        classList.push('addable')
+      }
+      return classList
+    },
+    titleExists() {
+      return this.title.length > 0
+    },
   },
   methods: {
-    addLisy:function () {
-      this.$store.dispatch("addlist",{title: this.title})
-      this.title = ""
-    }
-  },
+    addList: function() {
+      this.$store.dispatch('addlist', { title: this.title })
+      this.title = ''
+    },
+    startEditing: function() {
+      this.isEditing = true
+    },
+    finishEditing: function() {
+      this.isEditing = false
+    },
+  }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,51 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const savedLists = localStorage.getItem('trello-lists')　// ★①追加
+
+
+const store = new Vuex.Store({　// ★②編集
+    state: {
+        // ★ここから③編集
+        lists: savedLists ? JSON.parse(savedLists): [
+            {
+                title: 'Backlog',
+                cards: [
+                    { body: 'English' },
+                    { body: 'Mathematics' },
+                ]
+            },
+            {
+                title: 'Todo',
+                cards: [
+                    { body: 'Science' }
+                ]
+            },
+            {
+                title: 'Doing',
+                cards: []
+            }
+        ],
+        // ★ここまで③編集
+    },
+    mutations: {
+        addlist(state, payload) {
+            state.lists.push({ title: payload.title, cards:[] })
+        },
+    },
+    actions: {
+        addlist(context, payload) {
+            context.commit('addlist', payload)
+        },
+    },
+    getters: {
+    }
 })
+
+// ★ここから追記
+store.subscribe((mutation, state) => {
+    localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
+// ★ここまで追記
